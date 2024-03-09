69b18db1ece0ec9c0aa456ea2d0af971ad6ef093
MeghanaMohan <meghanamohan1704@gmail.com>
2024-03-08 20:00:13 -0500

    file is updated in the System integration folder

--- System integration/lab_4.py
+++ System integration/lab_4.py
@@ -28,6 +28,36 @@ time.sleep(5)
 assert "laptop" in driver.title
 
 # Selecting a laptop from the search results
-laptop_link = driver.find_element("xpath","/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div[1]/span/a/div/img")
+laptop_link = driver.find_element("xpath",'//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[12]/div/div/span/div/div/div[1]/span/a/div/img')
 laptop_link.click()
 
+
+
+
+# Waiting for the laptop details page to load
+time.sleep(2)
+
+# Adding the laptop to the cart
+add_to_cart_button = driver.find_element("id","add-to-cart-button")
+add_to_cart_button.click()
+
+# Waiting for the cart to update
+time.sleep(5)
+
+# Clicking on no thanks button
+# no_thanks_button= driver.find_element("xpath","/html/body/div[9]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
+# no_thanks_button.click()Y
+# time.sleep(2)
+
+proceed_to_checkout= driver.find_element("xpath","/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[3]/div/div[1]/form/span/span/span/input")
+proceed_to_checkout.click()
+time.sleep(2)
+
+
+# Verifying that the laptop has been added to the cart
+# cart_count = driver.find_element("id","nav-cart-count")
+# assert cart_count.text == "1"
+# cart_count.click()
+
+# Closing the webdriver
+driver.close()
